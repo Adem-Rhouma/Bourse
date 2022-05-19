27b1976adbdde1ed6b6c281a421986033d3a32d6
Adem_Rhouma <ademrhouma077@gmail.com>
2022-05-19 18:10:13 +0200

    andapp

--- anapp.py
+++ anapp.py
@@ -1,3 +1,5 @@
+
+from turtle import width
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
@@ -9,23 +11,50 @@ from kivy.uix.button import Button
 class MyGridLayout(GridLayout):
     def __init__(self, **kwargs):
         super(MyGridLayout, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text="name:  "))
-        self.name = TextInput(multiline=True)
-        self.add_widget(self.name)
+        self.cols = 1
+        
+        self.row_force_default=True
+        self.row_default_height=120
+        self.col_force_default=True
+        self.col_default_width=100
 
-        self.add_widget(Label(text="Favorite Pizza:   "))
+        self.top_grid = GridLayout(
+            row_force_default=True,
+            row_default_height=40,
+            col_force_default=True,
+            col_default_width=100)
+        self.top_grid.cols = 2
+        
+
+
+
+    
+        self.top_grid.add_widget(Label(text="name:  ",))
+        self.name = TextInput(multiline=True,)
+        self.top_grid.add_widget(self.name)
+
+        self.top_grid.add_widget(Label(text="Favorite Pizza:   "))
         self.pizza = TextInput(multiline=True)
-        self.add_widget(self.pizza)
+        self.top_grid.add_widget(self.pizza)
 
-        self.add_widget(Label(text="Color:  "))
+        self.top_grid.add_widget(Label(text="Color:  "))
         self.color = TextInput(multiline=True)
-        self.add_widget(self.color)
+        self.top_grid.add_widget(self.color)
+        self.add_widget(self.top_grid)
 
-
-        self.submit = Button(text="Submit", font_size =30)
+        self.submit = Button(text="Submit",
+            font_size =30,
+            size_hint_y= None,
+            height=50,
+            size_hint_x=None,
+            width=200
+            )
         self.submit.bind(on_press=self.press)
         self.add_widget(self.submit)
+
+        
+        
+        
     
     def press(self, instance):
         name = self.name.text
